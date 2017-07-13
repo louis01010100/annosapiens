51b16edde0bb5737f842d155f8ffd7f71eb4d23e
louis <louis01010100@gmail.com>
2017-07-14 00:19:52 +0800

    convert to python module

--- src/main/java/idv/louis/annosapiens/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package idv.louis.annosapiens;
-
-class HelloWorld {
-    public static void main(String[] arguments) {
-        System.out.println("Hello World");
-    }
-}
---
+++ vcfParser.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import vcf
+import sys
+
+
+def parse(filename):
+    vcfReader = vcf.Reader(filename=filename)
+
+    for record in vcfReader:
+        print(record)
+
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {cmd} vcfFilePath ".format(cmd = sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        parse(sys.argv[1])
+    except KeyboardInterrupt:
+        sys.exit(1)
+
+
